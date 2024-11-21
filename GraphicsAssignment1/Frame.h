971097c45035194208dd198ac88d956459007260
Zakhar-Khmurych <zakhar.khmurych@gmail.com>
2024-11-21 23:40:27 +0200

    Finished by requirements

--- GraphicsAssignment1/Frame.h
+++ GraphicsAssignment1/Frame.h
@@ -41,5 +41,68 @@ public:
             object.getGlobalBounds().intersects(leftLine.getGlobalBounds()) ||
             object.getGlobalBounds().intersects(rightLine.getGlobalBounds());
     }
+
+    bool checkCollision(const sf::FloatRect& rect) const {
+        return rect.intersects(topLine.getGlobalBounds()) ||
+            rect.intersects(bottomLine.getGlobalBounds()) ||
+            rect.intersects(leftLine.getGlobalBounds()) ||
+            rect.intersects(rightLine.getGlobalBounds());
+    }
 };
 
+
+class Square {
+private:
+    sf::RectangleShape squareShape;
+    float rotationAngle;
+    sf::Vector2f position;
+    float movementSpeed;
+    float rotationSpeed;
+
+public:
+    Square(sf::Vector2f size, sf::Vector2f startPosition, float moveSpeed, float rotateSpeed)
+        : rotationAngle(0.f), position(startPosition), movementSpeed(moveSpeed), rotationSpeed(rotateSpeed) {
+        squareShape.setSize(size);
+        squareShape.setFillColor(sf::Color::Green);
+        squareShape.setPosition(startPosition);
+    }
+
+    void move(float dx, float dy, float deltaTime) {
+        squareShape.move(dx * deltaTime, dy * deltaTime);
+        position = squareShape.getPosition();
+    }
+
+    void rotate(float angle, float deltaTime) {
+        rotationAngle += angle * deltaTime;
+        squareShape.setRotation(rotationAngle);
+    }
+
+    void setPosition(sf::Vector2f newPosition) {
+        position = newPosition;
+        squareShape.setPosition(newPosition);
+    }
+
+    void setRotation(float rotation) {
+        squareShape.setRotation(rotation);
+    }
+
+    void setFillColor(sf::Color color) {
+        squareShape.setFillColor(color);
+    }
+
+    sf::Vector2f getPosition() const {
+        return position;
+    }
+
+    float getRotation() {
+        return squareShape.getRotation();
+    }
+
+    sf::FloatRect getBounds() const {
+        return squareShape.getGlobalBounds();
+    }
+
+    void draw(sf::RenderWindow& window) {
+        window.draw(squareShape);
+    }
+};
--- GraphicsAssignment1/WindowRenderer.h
+++ GraphicsAssignment1/WindowRenderer.h
@@ -121,14 +121,16 @@ class WindowRenderer {
     float rotationAngle;
     sf::Vector2f position;
     InputHandler* inputHandler;
+    Square playerSquare;
 
-public:
-    WindowRenderer(int width, int height, InputHandler* handler)
-        : window_width(width), window_height(height), rotationAngle(0.f), position(0.f, 0.f), inputHandler(handler) {}
+    public:
+        WindowRenderer(int width, int height, InputHandler* handler)
+            : window_width(width), window_height(height), inputHandler(handler),
+            playerSquare(sf::Vector2f(150.f, 150.f), sf::Vector2f(375.f, 275.f), 150.f, 90.f) {}
 
 
 
-    void RunTheGame() {
+    void RunTheGame_FrameBased() {
         sf::RenderWindow window(sf::VideoMode(window_width, window_height), "SFML works!");
         Frame frame(window_width, window_height);
 
@@ -188,6 +190,134 @@ public:
             window.display();
         }
     }
+
+    void RunTheGame_interna () {
+        sf::RenderWindow window(sf::VideoMode(window_width, window_height), "SFML works!");
+        Frame frame(window_width, window_height);
+
+        sf::RectangleShape square(sf::Vector2f(150.f, 150.f));
+        square.setFillColor(sf::Color::Green);
+        square.setPosition(375.f, 275.f);
+
+        float movementSpeed = 450.f; 
+        float rotationSpeed = 270.f; 
+
+        sf::Clock clock; 
+
+        while (window.isOpen()) {
+            sf::Time elapsed = clock.restart(); 
+            float deltaTime = elapsed.asSeconds(); 
+
+            InputAction action = inputHandler->processInput(window);
+
+            if (action == InputAction::Exit) {
+                break;
+            }
+
+            switch (action) {
+            case InputAction::MoveUp:
+                square.move(0.f, -movementSpeed * deltaTime);
+                break;
+            case InputAction::MoveDown:
+                square.move(0.f, movementSpeed * deltaTime);
+                break;
+            case InputAction::MoveLeft:
+                square.move(-movementSpeed * deltaTime, 0.f);
+                break;
+            case InputAction::MoveRight:
+                square.move(movementSpeed * deltaTime, 0.f);
+                break;
+            case InputAction::RotateClockwise:
+                rotationAngle += rotationSpeed * deltaTime;
+                break;
+            case InputAction::RotateCounterClockwise:
+                rotationAngle -= rotationSpeed * deltaTime;
+                break;
+            default:
+                break;
+            }
+
+            square.setRotation(rotationAngle);
+
+            if (frame.checkCollision(square)) {
+                square.setPosition(position);
+                square.setFillColor(sf::Color::Red);
+                movementSpeed = 50.f; 
+            }
+            else {
+                square.setFillColor(sf::Color::Green);
+                movementSpeed = 150.f;
+            }
+
+            position = square.getPosition();
+
+            window.clear();
+            frame.draw(window);
+            window.draw(square);
+            window.display();
+        }
+    }
+
+    void RunTheGame() {
+        sf::RenderWindow window(sf::VideoMode(window_width, window_height), "SFML works!");
+        Frame frame(window_width, window_height);
+
+        sf::Clock clock;
+        sf::Vector2f previousPosition = playerSquare.getPosition();
+        float previousRotation = playerSquare.getRotation();
+
+        while (window.isOpen()) {
+            sf::Time elapsed = clock.restart();
+            float deltaTime = elapsed.asSeconds();
+
+            InputAction action = inputHandler->processInput(window);
+
+            if (action == InputAction::Exit) {
+                break;
+            }
+
+            previousPosition = playerSquare.getPosition();
+            previousRotation = playerSquare.getRotation();
+
+            switch (action) {
+            case InputAction::MoveUp:
+                playerSquare.move(0.f, -100.f, deltaTime);
+                break;
+            case InputAction::MoveDown:
+                playerSquare.move(0.f, 100.f, deltaTime);
+                break;
+            case InputAction::MoveLeft:
+                playerSquare.move(-100.f, 0.f, deltaTime);
+                break;
+            case InputAction::MoveRight:
+                playerSquare.move(100.f, 0.f, deltaTime);
+                break;
+            case InputAction::RotateClockwise:
+                playerSquare.rotate(90.f, deltaTime);
+                break;
+            case InputAction::RotateCounterClockwise:
+                playerSquare.rotate(-90.f, deltaTime);
+                break;
+            default:
+                break;
+            }
+
+            if (frame.checkCollision(playerSquare.getBounds())) {
+                playerSquare.setPosition(previousPosition);
+                playerSquare.setRotation(previousRotation);
+                playerSquare.setFillColor(sf::Color::Red);
+            }
+            else {
+                playerSquare.setFillColor(sf::Color::Green);
+            }
+
+            window.clear();
+            frame.draw(window);
+            playerSquare.draw(window);
+            window.display();
+        }
+    }
+
 };
 
 
